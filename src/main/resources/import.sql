ce5766548b5e32be0a5125f3df2f03a97d817405
user2019 <user@tecsup.edu.pe>
2019-05-30 01:01:10 -0500

    fixing code

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tabla productos */
+INSERT INTO productos (id, item, marca, modelo, producto, codigo) VALUES(1, '001', 'ANDADOR MONO', 'MONO', 'I001', 'AN-MO-I001');
+INSERT INTO productos (id, item, marca, modelo, producto, codigo) VALUES(2, '002', 'ANDADOR PATO', 'PATO', 'I002', 'AN-PA-I002');
